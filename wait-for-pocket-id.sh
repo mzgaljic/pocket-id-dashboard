919038a67e55558b30033517d1f3b94835f8076a
Mark Zgaljic <mzgaljic@me.com>
2025-03-31 01:34:19 -1000

    Fix custom entrypoint script

--- wait-for-pocket-id.sh
+++ wait-for-pocket-id.sh
@@ -6,24 +6,28 @@ TIMEOUT=15
 DELAY=3
 START_TIME=$(date +%s)
 
-echo "Checking for readiness of Pocket-ID ($POCKET_ID_BASE_URL)"
+echo "Checking for readiness of Pocket ID ($POCKET_ID_BASE_URL)"
 
 while true; do
-    HTTP_STATUS=$(wget --spider --server-response --quiet "$POCKET_ID_CONFIG_URL" 2>&1 | grep "HTTP/" | awk '{print $2}')
+  HTTP_STATUS=$(wget --spider --server-response --quiet "$POCKET_ID_CONFIG_URL" 2>&1 | awk '/HTTP\/[0-9.]+/ {print $2; exit}')
 
-  if [ "$HTTP_STATUS" -eq 200 ]; then
-    echo "Pocket-ID API is ready!"
+  if [ "$HTTP_STATUS" = "200" ]; then
+    echo "Pocket ID API is ready!"
     break
+  elif [ "$HTTP_STATUS" = "" ]; then
+    echo "No HTTP response received."
+  else
+    echo "Received HTTP status: $HTTP_STATUS"
   fi
 
   # Check the elapsed time
   ELAPSED_TIME=$(($(date +%s) - START_TIME))
   if [ "$ELAPSED_TIME" -ge "$TIMEOUT" ]; then
-    echo "Timeout reached. Pocket-ID API is not ready after $TIMEOUT seconds."
+    echo "Timeout reached. Pocket ID API is not ready after $TIMEOUT seconds."
     break
   fi
 
-  echo "Pocket-ID api not ready, retrying in $DELAY seconds..."
+  echo "Pocket ID api not ready, retrying in $DELAY seconds..."
   sleep "$DELAY"
 done
 
